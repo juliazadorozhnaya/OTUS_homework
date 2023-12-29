183e7506b722c81d7eaaec9e2950a41663f050b4
y.zadorozhnaya <j.zadorozhnaya2707@gmail.com>
2023-12-29 15:04:55 +0300

    add: сделано второе дз

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,58 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	var builder strings.Builder
+	runeSlice := []rune(s)
+
+	for i := 0; i < len(runeSlice); i++ {
+		current := runeSlice[i]
+
+		if unicode.IsLetter(current) || (current == '\\' && i+1 < len(runeSlice) && runeSlice[i+1] == '\\') {
+			builder.WriteRune(current)
+		} else if current == '\\' {
+			i++
+			if i >= len(runeSlice) {
+				return "", ErrInvalidString
+			}
+
+			next := runeSlice[i]
+			if unicode.IsDigit(next) || next == '\\' {
+				builder.WriteRune(next)
+			} else {
+				return "", ErrInvalidString
+			}
+		} else if unicode.IsDigit(current) {
+			if i == 0 || !(unicode.IsLetter(runeSlice[i-1]) || runeSlice[i-1] == '\\') {
+				return "", ErrInvalidString
+			}
+
+			repeatCount, err := strconv.Atoi(string(current))
+			if err != nil {
+				return "", ErrInvalidString
+			}
+
+			prev := runeSlice[i-1]
+			if repeatCount > 1 {
+				builder.WriteString(strings.Repeat(string(prev), repeatCount-1))
+			} else if repeatCount == 0 {
+				result := builder.String()
+				if len(result) > 0 {
+					builder.Reset()
+					builder.WriteString(result[:len(result)-1])
+				}
+			}
+		} else {
+			return "", ErrInvalidString
+		}
+	}
+
+	return builder.String(), nil
 }
