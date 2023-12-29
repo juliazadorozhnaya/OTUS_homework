ee3244c002dc905557f6b1b3acd6464b10e08546
y.zadorozhnaya <j.zadorozhnaya2707@gmail.com>
2023-12-29 16:09:59 +0300

    add: сделано второе дз

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -16,44 +16,69 @@ func Unpack(s string) (string, error) {
 	for i := 0; i < len(runeSlice); i++ {
 		current := runeSlice[i]
 
-		if unicode.IsLetter(current) || (current == '\\' && i+1 < len(runeSlice) && runeSlice[i+1] == '\\') {
+		switch {
+		case isLetterOrEscapedBackslash(current, i, runeSlice):
 			builder.WriteRune(current)
-		} else if current == '\\' {
-			i++
-			if i >= len(runeSlice) {
-				return "", ErrInvalidString
-			}
-
-			next := runeSlice[i]
-			if unicode.IsDigit(next) || next == '\\' {
-				builder.WriteRune(next)
-			} else {
-				return "", ErrInvalidString
-			}
-		} else if unicode.IsDigit(current) {
-			if i == 0 || !(unicode.IsLetter(runeSlice[i-1]) || runeSlice[i-1] == '\\') {
-				return "", ErrInvalidString
-			}
-
-			repeatCount, err := strconv.Atoi(string(current))
+		case current == '\\':
+			next, err := handleBackslash(i, runeSlice)
 			if err != nil {
-				return "", ErrInvalidString
+				return "", err
 			}
-
-			prev := runeSlice[i-1]
-			if repeatCount > 1 {
-				builder.WriteString(strings.Repeat(string(prev), repeatCount-1))
-			} else if repeatCount == 0 {
-				result := builder.String()
-				if len(result) > 0 {
-					builder.Reset()
-					builder.WriteString(result[:len(result)-1])
-				}
+			builder.WriteRune(next)
+		case unicode.IsDigit(current):
+			err := handleDigit(current, i, runeSlice, &builder)
+			if err != nil {
+				return "", err
 			}
-		} else {
+		default:
 			return "", ErrInvalidString
 		}
 	}
 
 	return builder.String(), nil
+}
+
+func isLetterOrEscapedBackslash(current rune, i int, runeSlice []rune) bool {
+	return unicode.IsLetter(current) || (current == '\\' && i+1 < len(runeSlice) && runeSlice[i+1] == '\\')
+}
+
+func handleBackslash(i int, runeSlice []rune) (rune, error) {
+	i++
+	if i >= len(runeSlice) {
+		return 0, ErrInvalidString
+	}
+	next := runeSlice[i]
+	if unicode.IsDigit(next) || next == '\\' {
+		return next, nil
+	}
+	return 0, ErrInvalidString
+}
+
+func handleDigit(current rune, i int, runeSlice []rune, builder *strings.Builder) error {
+	if i == 0 || !(unicode.IsLetter(runeSlice[i-1]) || runeSlice[i-1] == '\\') {
+		return ErrInvalidString
+	}
+	repeatCount, err := strconv.Atoi(string(current))
+	if err != nil {
+		return ErrInvalidString
+	}
+	prev := runeSlice[i-1]
+	processRepeat(repeatCount, prev, builder)
+	return nil
+}
+
+func processRepeat(repeatCount int, prev rune, builder *strings.Builder) {
+	if repeatCount > 1 {
+		builder.WriteString(strings.Repeat(string(prev), repeatCount-1))
+	} else if repeatCount == 0 {
+		removeLastRuneIfAny(builder)
+	}
+}
+
+func removeLastRuneIfAny(builder *strings.Builder) {
+	result := builder.String()
+	if len(result) > 0 {
+		builder.Reset()
+		builder.WriteString(result[:len(result)-1])
+	}
 }
