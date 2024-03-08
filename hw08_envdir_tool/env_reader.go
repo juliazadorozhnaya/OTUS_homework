3cefad569d4c05044064b7f951d91196a588494d
y.zadorozhnaya <j.zadorozhnaya2707@gmail.com>
2024-03-08 13:57:49 +0300

    HW08 is completed

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,6 +1,18 @@
 package main
 
+import (
+	"bufio"
+	"errors"
+	"io/fs"
+	"log"
+	"os"
+	"path"
+	"strings"
+)
+
 type Environment map[string]EnvValue
+
+var ErrInvalidFilename = errors.New("invalid filename")
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
 type EnvValue struct {
@@ -11,6 +23,70 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	dirContent, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, errors.New("failed to read dir")
+	}
+
+	env := make(Environment)
+	for _, fileInfo := range dirContent {
+		if fileInfo.IsDir() {
+			continue
+		}
+
+		if strings.Contains(fileInfo.Name(), "=") {
+			return nil, ErrInvalidFilename
+		}
+
+		var info fs.FileInfo
+		info, err = fileInfo.Info()
+		if err != nil {
+			return nil, err
+		}
+		if info.Size() == 0 {
+			env[fileInfo.Name()] = EnvValue{
+				Value:      "",
+				NeedRemove: true,
+			}
+			continue
+		}
+
+		val, err := getFirstValueFromFile(path.Join(dir, fileInfo.Name()))
+		if err != nil {
+			return nil, err
+		}
+
+		env[fileInfo.Name()] = EnvValue{
+			Value:      val,
+			NeedRemove: false,
+		}
+	}
+
+	return env, nil
+}
+
+func getFirstValueFromFile(fileName string) (string, error) {
+	f, err := os.Open(fileName)
+	if err != nil {
+		return "", errors.New("failed to open file")
+	}
+	defer func() {
+		err = f.Close()
+		if err != nil {
+			log.Println("failed to close file: %w", err)
+		}
+	}()
+
+	buf := bufio.NewScanner(f)
+	buf.Scan()
+	firstLine := buf.Text()
+
+	if err := buf.Err(); err != nil {
+		return "", errors.New("err in scan")
+	}
+
+	val := strings.ReplaceAll(firstLine, "\x00", "\n")
+	val = strings.TrimRight(val, " \t\n")
+
+	return val, nil
 }
--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,40 @@
 package main
 
+import (
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
-func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+func RunCmd(args []string, env Environment) (returnCode int) {
+	if len(args) == 0 {
+		return -1
+	}
+
+	for envVarName, envInfo := range env {
+		if err := os.Unsetenv(envVarName); err != nil {
+			return -1
+		}
+
+		if !envInfo.NeedRemove {
+			if err := os.Setenv(envVarName, envInfo.Value); err != nil {
+				return -1
+			}
+		}
+	}
+
+	name := args[0]
+	command := args[1:]
+
+	cmd := exec.Command(name, command...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Env = os.Environ()
+
+	err := cmd.Run()
+	if err != nil {
+		return -1
+	}
+	return 0
 }
